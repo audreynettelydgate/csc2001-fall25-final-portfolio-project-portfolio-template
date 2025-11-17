6e38e882bb1423630236e08a2c9cb568ff099c25
audreynette.lydgate <audreynette.lydgate@gmail.com>
2025-11-17 13:21:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('themeToggle')?.addEventListener('click', () => {
   if (!el) return;
 
   try {
-    const res = await fetch('https://api.quotable.io/random');
+    const res = await fetch('https://www.minimizemymess.com/random-quote-generator?srsltid=AfmBOop3Rq23oHVhko7B3E5YB0ODW82Sh0a5GtKZfU3ZC4GW0YQeZn3P');
     const data = await res.json();
     el.textContent = `“${data.content}” — ${data.author}`;
   } catch {
